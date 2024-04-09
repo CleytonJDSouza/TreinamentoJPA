b1f3af7a6e3a6ff7724147c4dfb4242bd749bb73
Cleyton <cleytonjosedesouza@hotmail.com>
2024-04-09 16:23:38 -0300

    Aula 4

--- src/main/java/br/com/alura/loja/modelo/Cliente.java
+++ src/main/java/br/com/alura/loja/modelo/Cliente.java
@@ -1,5 +1,6 @@
 package br.com.alura.loja.modelo;
 
+import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -13,37 +14,26 @@ public class Cliente {
      @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String nome;
-    private String cpf;
 
+    @Embedded
+    private DadosPessoais dadosPessoais;
+    
     public Cliente(String nome, String cpf) {
-        this.nome = nome;
-        this.cpf = cpf;
+        this.dadosPessoais = new DadosPessoais(nome, cpf);
     }
+
     public Cliente() {
     }
 
     public Long getId() {
         return id;
     }
-    
-    public String getNome() {
-        return nome;
-    }
-
-    public String getCpf() {
-        return cpf;
-    }
 
     public void setId(Long id) {
         this.id = id;
     }
 
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public void setCpf(String cpf) {
-        this.cpf = cpf;
+    public DadosPessoais getDadosPessoais() {
+        return dadosPessoais;
     }
 }
