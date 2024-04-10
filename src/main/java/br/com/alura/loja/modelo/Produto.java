8fe701df42310c70ad3bfaa9151d09d2d357635e
Cleyton <cleytonjosedesouza@hotmail.com>
2024-04-10 09:23:43 -0300

    Heranças

--- src/main/java/br/com/alura/loja/modelo/Produto.java
+++ src/main/java/br/com/alura/loja/modelo/Produto.java
@@ -7,6 +7,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
 import javax.persistence.ManyToOne;
 import javax.persistence.NamedQuery;
 import javax.persistence.Table;
@@ -15,6 +17,7 @@ import javax.persistence.Table;
 @Table(name = "produtos")
 @NamedQuery(name = "Produto.produdosPorCategoria", 
 query = "SELECT p FROM Produto p WHERE p.categoria.nome = :nome")
+@Inheritance(strategy = InheritanceType.JOINED)
 public class Produto {
     
     @Id
